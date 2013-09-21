cbdac1ee31ce113a5abba1ee3ada3832e76cd09c
Greg Hild <grhild@gmail.com>
2013-09-21 17:59:44 -0500

    Delete html and replaced with embedded ruby file. Add a post facebook to test.rb and Add a result.erb file.

--- test.rb
+++ test.rb
@@ -1,6 +1,22 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'sinatra/reloader'
 require 'koala'
+
+equire_relative './lib/social_one.rb'
 
 graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
 graph.put_connections('me', 'feed', :message => "App Works!")
+
+
+get '/' do
+  erb :socialone
+end
+
+post '/facebook'
+  erb :result
+end
+
 
 puts graph.inspect
