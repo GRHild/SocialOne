c9f7911425aa68cefe60adbe2a19e68cec19f272
MakerSquare Student <students@themakersquare.com>
2013-09-21 18:49:54 -0500

    twitter post not working

--- test.rb
+++ test.rb
@@ -1,8 +1,29 @@
 require 'koala'
+require 'twitter'
 
-graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
-graph.put_connections('me', 'feed', :message => "another test")
+# graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
+# graph.put_connections('me', 'feed', :message => "yet another test")
+
+class Twit
+	def initialize
+		client = Twitter::Client.new.configure do |config|
+		  config.consumer_key        = ENV['CONSUMER_KEY']
+		  config.consumer_secret     = ENV['CONSUMER_SECRET']
+		  config.oauth_token         = ENV['OAUTH_TOKEN']
+		  config.oauth_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+		end
+	end
+
+	def twit(tweet)
+		Twitter.update(tweet)
+	end
+end
+
+# Twitter.update("test")
+
+twit1 = Twit.new
+twit1.twit('test')
 
 
-puts graph.inspect
+# puts graph.inspect
 # puts ENV['ACCESS_TOKEN']
