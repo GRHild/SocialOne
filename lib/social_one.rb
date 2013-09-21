d0a417cebd02767c78cf944c4fdc2f3667258078
Greg Hild <grhild@gmail.com>
2013-09-21 13:35:05 -0500

    Add an html file to the ruby file.

---
+++ lib/social_one.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'koala'
+
+module SocialOne
+
+	class User
+		def initialize
+			@graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
+		end
+
+		def post_fb_text(status_update)
+			@graph.put_connections('me', 'feed', :message => status_update)
+		end
+	end
+
+end
---
+++ spec/social_one_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative './lib/social_one.rb'
+	
+# describe SocialOne::User do
+# 	describe
+# end
+
+# describe SocialOne::Post do
+# 	describe '#post_facebook' do
+# 		context 'posts a string to facebook'
+# 			it 'returns a string to be posted to facebook' 
+# 		end
+# 	end
+# end
