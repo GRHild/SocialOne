ba4d4e5525b1ac1cc25b6a5435ca2140b74a0df2
MakerSquare Student <students@themakersquare.com>
2013-09-22 15:56:03 -0500

    add twitter to test.rb

--- test.rb
+++ test.rb
@@ -1,29 +1,16 @@
+require 'rubygems'
 require 'koala'
 require 'twitter'
 
 # graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
-# graph.put_connections('me', 'feed', :message => "yet another test")
+# graph.put_connections('me', 'feed', :message => "still more testing")
 
-class Twit
-	def initialize
-		client = Twitter::Client.new.configure do |config|
-		  config.consumer_key        = ENV['CONSUMER_KEY']
-		  config.consumer_secret     = ENV['CONSUMER_SECRET']
-		  config.oauth_token         = ENV['OAUTH_TOKEN']
-		  config.oauth_token_secret  = ENV['OAUTH_TOKEN_SECRET']
-		end
-	end
-
-	def twit(tweet)
-		Twitter.update(tweet)
-	end
+client = Twitter::Client.new.configure do |config|
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.oauth_token         = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret  = ENV['OAUTH_TOKEN_SECRET']
 end
 
-# Twitter.update("test")
+client.update('another test')
 
-twit1 = Twit.new
-twit1.twit('test')
-
-
-# puts graph.inspect
-# puts ENV['ACCESS_TOKEN']
